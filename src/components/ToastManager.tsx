2cae23198275cd07867fa288149df4a99bd4040e
Babalola Macaulay <babs@babs.dev>
2024-03-26 17:27:00 +0100

    chore: changed animated component to react-native-reanimated

--- src/components/ToastManager.tsx
+++ src/components/ToastManager.tsx
@@ -1,4 +1,4 @@
-import { Animated, Dimensions, StyleSheet } from 'react-native'
+import { Animated, Dimensions } from 'react-native'
 import React, { Component } from 'react'
 import { Gesture, type GestureUpdateEvent, type PanGestureHandlerEventPayload } from 'react-native-gesture-handler';
 import Toast from './Toast'
